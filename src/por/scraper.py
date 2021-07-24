d2303658b0632f1dbc2d63aca58f362fa649e6cf
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-07-25 00:53:47 +0100

    ✨ Add Chapter 10 expected file

--- src/por/scraper.py
+++ src/por/scraper.py
@@ -272,7 +272,7 @@ if __name__ == '__main__':
     #     p.write_text(requests.get("https://www.scouts.org.uk" + link).content.decode("utf-8"), encoding="utf-8")
 
     # chapters = [*range(1, 15+1)]
-    chapters = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+    chapters = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     for i in chapters:
         raw = Path(f"ch{i}-raw.txt").read_text(encoding="utf-8")
         exp = Path(f"chapter-{i}.exp.rst")  # expected
@@ -355,6 +355,7 @@ if __name__ == '__main__':
 #   emphasis inside links / vice versa (lots of examples in chapter 9)
 #   --- add manual unity callout link thing
 #   8.1(e) unity "call to action" box
+#   10.20, 10."Uniform Diagrams" -- link to PDF etc, or transclude
 
 
 # TODO snags:
