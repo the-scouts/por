6a41dde5afdf332e2c0bc95e95c0b032c8b5bdd6
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-07-25 01:00:57 +0100

    ✨ Add Chapter 13 expected file

--- src/por/scraper.py
+++ src/por/scraper.py
@@ -272,7 +272,7 @@ if __name__ == '__main__':
     #     p.write_text(requests.get("https://www.scouts.org.uk" + link).content.decode("utf-8"), encoding="utf-8")
 
     # chapters = [*range(1, 15+1)]
-    chapters = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+    chapters = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
     for i in chapters:
         raw = Path(f"ch{i}-raw.txt").read_text(encoding="utf-8")
         exp = Path(f"chapter-{i}.exp.rst")  # expected
